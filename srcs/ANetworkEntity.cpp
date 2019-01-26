ecb911c2e84bed1e7a82ac8343f19444af3b50a4
Andy <AndyShih51@gmail.com>
2019-01-25 16:41:48 -0800

    try this

--- srcs/ANetworkEntity.cpp
+++ srcs/ANetworkEntity.cpp
@@ -24,36 +24,66 @@ void			ANetworkEntity::receiveMessages(void)
 	sf::Int8	message_;
 	e_message	message;
 
-	while ((status = this->_socket.receive(packet)) != sf::Socket::NotReady)
+	// while ((status = this->_socket.receive(packet)) != sf::Socket::Disconnected)
+	// {
+	// 	if (status == sf::Socket::Done)
+	// 	{
+	// 		if (!(packet >> message_))
+	// 		{
+	// 			// error, 🤷
+	// 			printf(" packet >> messageHeader_ FAILED \n");
+	// 		}
+
+	// 		message = static_cast<e_message>(message_);
+
+	// 		switch (message)
+	// 		{
+	// 			case P1_TURN_LEFT:
+	// 				this->_nibbler.turnLeftP1();
+	// 				break;
+	// 			case P1_TURN_RIGHT:
+	// 				this->_nibbler.turnRightP1();
+	// 				break;
+	// 			case P2_TURN_LEFT:
+	// 				this->_nibbler.turnLeftP2();
+	// 				break;
+	// 			case P2_TURN_RIGHT:
+	// 				this->_nibbler.turnRightP2();
+	// 				break;
+	// 			default:
+	// 				printf(" error 🤷 \n");
+	// 				break;
+	// 		}
+	// 	}
+	// }
+
+	if ((status = this->_socket.receive(packet)) == sf::Socket::Done)
 	{
-		if (status == sf::Socket::Done)
+		if (!(packet >> message_))
 		{
-			if (!(packet >> message_))
-			{
-				// error, 🤷
-				printf(" packet >> messageHeader_ FAILED \n");
-			}
+			// error, 🤷
+			printf(" packet >> messageHeader_ FAILED \n");
+		}
 
-			message = static_cast<e_message>(message_);
+		message = static_cast<e_message>(message_);
 
-			switch (message)
-			{
-				case P1_TURN_LEFT:
-					this->_nibbler.turnLeftP1();
-					break;
-				case P1_TURN_RIGHT:
-					this->_nibbler.turnRightP1();
-					break;
-				case P2_TURN_LEFT:
-					this->_nibbler.turnLeftP2();
-					break;
-				case P2_TURN_RIGHT:
-					this->_nibbler.turnRightP2();
-					break;
-				default:
-					printf(" error 🤷 \n");
-					break;
-			}
+		switch (message)
+		{
+			case P1_TURN_LEFT:
+				this->_nibbler.turnLeftP1();
+				break;
+			case P1_TURN_RIGHT:
+				this->_nibbler.turnRightP1();
+				break;
+			case P2_TURN_LEFT:
+				this->_nibbler.turnLeftP2();
+				break;
+			case P2_TURN_RIGHT:
+				this->_nibbler.turnRightP2();
+				break;
+			default:
+				printf(" error 🤷 \n");
+				break;
 		}
 	}
 }
